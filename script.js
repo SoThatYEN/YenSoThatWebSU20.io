496c0a563d74e4b64d9e7397f07c15bf4f16e2ae
SoThatYEN <yensothat99@gmail.com>
2025-03-11 20:17:36 +0700

    first commit

--- script.js
+++ script.js
@@ -1,38 +1,44 @@
-function submitForm(event) {
-    event.preventDefault(); // Prevent actual form submission
-  
-    // Get input values
-    document.getElementById("displayName").innerText =
-      document.getElementById("name").value;
-    document.getElementById("displayEmail").innerText =
-      document.getElementById("email").value;
-  
-    // Format salary value
-    const salary = parseFloat(document.getElementById("salary").value);
-    const formattedSalary = new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-    }).format(salary);
-    document.getElementById("displaySalary").innerText = formattedSalary;
-  
-    document.getElementById("displayHireDate").innerText =
-      document.getElementById("hireDate").value;
-  
-    // Show success alert
-    document.getElementById("successAlert").classList.remove("d-none");
-  
-    // Reset form fields
-    alert ("1.EmpName : " + document.getElementById("name").value + "\n"+ 
-        "2.Email : " + document.getElementById("email").value + "\n"+
-        "3.Salary : " + `$${parseFloat(document.getElementById("salary").value).toLocaleString(undefined, {minimumFractionDigits: 2})}` + "\n"+
-        "4.HireDate " + document.getElementById("hireDate").value + "\n"
-      )
+event.preventDefault(); // Prevent actual form submission
 
-    document.getElementById("myForm").reset();
+  // Get input values
+  document.getElementById("displayName").innerText =
+    document.getElementById("name").value;
+  document.getElementById("displayEmail").innerText =
+    document.getElementById("email").value;
 
+  // Format salary value
+  const salary = parseFloat(document.getElementById("salary").value);
+  const formattedSalary = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(salary);
+  document.getElementById("displaySalary").innerText = formattedSalary;
+
+  document.getElementById("displayHireDate").innerText =
+    document.getElementById("hireDate").value;
+
+  // Show success alert
+  document.getElementById("successAlert").classList.remove("d-none");
+  // Show alert message
+  alert(
+    "1.EmpName: " +
+      document.getElementById("displayName").innerText +
+      "\n" +
+      "2.Email: " +
+      document.getElementById("displayEmail").innerText +
+      "\n" +
+      "3.Salary: " +
+      document.getElementById("displaySalary").innerText +
+      " \n" +
+      "4.HireDate: " +
+      document.getElementById("displayHireDate").innerText +
+      "\n"
+  );
+  // Reset form fields
+  document.getElementById("myForm").reset();
 }
-  
-  function hideAlert() {
-    document.getElementById("successAlert").classList.add("d-none");
-  }
+
+function hideAlert() {
+  document.getElementById("successAlert").classList.add("d-none");
+}
 
